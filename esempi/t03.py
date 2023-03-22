8fddb47bce97eebf4c2179c6852a924f5f7806f6
franco-rcr-01 <franco.rcr@gmail.com>
2023-03-22 14:49:56 +0100

    Create t03.py

---
+++ esempi/t03.py
@@ -0,0 +1,17 @@
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_iris
+
+# carica il dataset iris
+iris = load_iris()
+
+# inizializza il modello K-means con k=3
+kmeans = KMeans(n_clusters=3, n_init="auto")
+
+# addestra il modello con il dataset iris
+kmeans.fit(iris.data)
+
+# effettua una predizione sui campioni del dataset
+predicted_labels = kmeans.predict(iris.data)
+
+print("Etichette predette:", predicted_labels)
+print("Etichette del set :", iris.target)
